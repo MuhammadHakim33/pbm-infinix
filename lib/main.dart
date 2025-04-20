8369b18f33039f96e2fd7b651a2dac8cfc43a044
MuhammadHakim33 <mhmd.hakim53@gmail.com>
2025-04-20 16:11:43 +0700

    retrive data from textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomePage(),
+      '/message': (context) => const MessagePage(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +33,13 @@ class MyApp extends StatelessWidget {
             Container(
               height: 60,
               width: double.infinity,
-              margin: EdgeInsets.all(20),
+              margin: EdgeInsets.all(15),
               alignment: Alignment.center,
               decoration: BoxDecoration(
                 color: Colors.blue,
                 borderRadius: BorderRadius.circular(3),
               ),
-              transform: Matrix4.rotationZ(0.03),
+              transform: Matrix4.rotationZ(0.01),
               child: Text(
                 'Muhammad Hakim',
                 style: TextStyle(
@@ -44,13 +50,13 @@ class MyApp extends StatelessWidget {
             Container(
               height: 60,
               width: double.infinity,
-              margin: EdgeInsets.all(20),
+              margin: EdgeInsets.all(15),
               alignment: Alignment.center,
               decoration: BoxDecoration(
                 color: Colors.green,
                 borderRadius: BorderRadius.circular(3),
               ),
-              transform: Matrix4.rotationZ(-0.03),
+              transform: Matrix4.rotationZ(-0.01),
               child: Text(
                 'Muhammad Rachel Fathan Idzhany',
                 style: TextStyle(
@@ -61,13 +67,13 @@ class MyApp extends StatelessWidget {
             Container(
               height: 60,
               width: double.infinity,
-              margin: EdgeInsets.all(20),
+              margin: EdgeInsets.all(15),
               alignment: Alignment.center,
               decoration: BoxDecoration(
                 color: Colors.deepPurpleAccent,
                 borderRadius: BorderRadius.circular(3),
               ),
-              transform: Matrix4.rotationZ(0.03),
+              transform: Matrix4.rotationZ(0.01),
               child: Text(
                 'Muhammad Azhar Rabbani',
                 style: TextStyle(
@@ -78,13 +84,13 @@ class MyApp extends StatelessWidget {
             Container(
               height: 60,
               width: double.infinity,
-              margin: EdgeInsets.all(20),
+              margin: EdgeInsets.all(15),
               alignment: Alignment.center,
               decoration: BoxDecoration(
                 color: Colors.deepOrangeAccent,
                 borderRadius: BorderRadius.circular(3),
               ),
-              transform: Matrix4.rotationZ(-0.03),
+              transform: Matrix4.rotationZ(-0.01),
               child: Text(
                 'Dhandi Adam',
                 style: TextStyle(
@@ -95,13 +101,13 @@ class MyApp extends StatelessWidget {
             Container(
               height: 60,
               width: double.infinity,
-              margin: EdgeInsets.all(20),
+              margin: EdgeInsets.all(15),
               alignment: Alignment.center,
               decoration: BoxDecoration(
                 color: Colors.redAccent,
                 borderRadius: BorderRadius.circular(3),
               ),
-              transform: Matrix4.rotationZ(0.03),
+              transform: Matrix4.rotationZ(0.01),
               child: Text(
                 'Antonius Valentino Dharma Kusuma',
                 style: TextStyle(
@@ -109,6 +115,28 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
             ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              height: 40,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 33, 52, 72),
+                  elevation: 1,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)
+                  )
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/message');
+                }, 
+                child: Text(
+                  'Tulis Pesan',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
@@ -116,3 +144,120 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MessagePage extends StatefulWidget {
+  const MessagePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MessageState createState() => _MessageState();
+}
+
+class _MessageState extends State<MessagePage> {
+
+  late String name;
+  late String alamat;
+  late String pesan;
+
+  String message = '';
+
+  void submit() {
+    setState(() {
+      message = '''
+        Pengirim : $name
+        Alamat : $alamat
+        Pesan : $pesan
+      ''';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Infinix - Message',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Message', 
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+              )
+          ),
+          backgroundColor: const Color.fromARGB(255, 33, 52, 72),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                onChanged: (value) => name = value,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Alamat'),
+                onChanged: (value) => alamat = value,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Pesan'),
+                onChanged: (value) => pesan = value,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 2, 88, 180),
+                  elevation: 1,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)
+                  )
+                ),
+                onPressed: submit, 
+                child: Text(
+                  'Kirim',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: Text(message),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              height: 40,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 33, 52, 72),
+                  elevation: 1,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)
+                  )
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                }, 
+                child: Text(
+                  'Kembali',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
