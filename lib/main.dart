8a1a01fc4bff52549cc293486e93a9482ef02638
MuhammadHakim33 <mhmd.hakim53@gmail.com>
2025-04-28 07:17:17 +0700

    db insert

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
     initialRoute: '/',
     routes: {
       '/': (context) => const HomePage(),
+      '/message': (context) => const MessagePage(),
     },
   ));
 }
@@ -82,37 +83,66 @@ class _HomePage extends State<HomePage> {
           ),
           backgroundColor: const Color.fromARGB(255, 33, 52, 72),
         ),
-        body: isLoading 
-            ? CircularProgressIndicator()
-            : ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailPage(id: data[index]['id']),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    height: 60,
-                    width: double.infinity,
-                    margin: const EdgeInsets.all(15),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(3),
+        body: isLoading
+          ? const Center(child: CircularProgressIndicator()) // <-- Tambah Center supaya loadingnya di tengah
+          : Column(
+              children: [
+                 Container(
+                  margin: EdgeInsets.only(top: 10),
+                  height: 40,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 33, 52, 72),
+                      elevation: 1,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(3)
+                      )
                     ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/message');
+                    }, 
                     child: Text(
-                      data[index]["nama"],
-                      style: const TextStyle(color: Colors.white),
+                      'Tulis Pesan',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
                     ),
                   ),
-                );
-              }
-            )
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailPage(id: data[index]['id']),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          height: 60,
+                          width: double.infinity,
+                          margin: const EdgeInsets.all(15),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(3),
+                          ),
+                          child: Text(
+                            data[index]["nama"],
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
       ),
     );
   }
@@ -170,6 +200,135 @@ class _DetailPageState extends State<DetailPage> {
                     style: const TextStyle(fontSize: 24),
                   ),
                 ),
+    );
+  }
+}
+
+class MessagePage extends StatefulWidget {
+  const MessagePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MessageState createState() => _MessageState();
+}
+
+class _MessageState extends State<MessagePage> {
+
+  late String name;
+  late String alamat;
+  late String pesan;
+
+  String message = '';
+
+  var url = "https://tugas-pbm-caf58-default-rtdb.asia-southeast1.firebasedatabase.app/" + "data.json";
+
+  void submit() async {
+    message = '''
+      Pengirim : $name
+      Alamat : $alamat
+      Pesan : $pesan
+    ''';
+
+    try {
+      // Send a POST request to the database with the title data
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode({"message": message}),
+      );
+    } catch (error) {
+      // Handle any errors that occur during the request
+      throw error;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Infinix - Message',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Message', 
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+              )
+          ),
+          backgroundColor: const Color.fromARGB(255, 33, 52, 72),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                onChanged: (value) => name = value,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Alamat'),
+                onChanged: (value) => alamat = value,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Pesan'),
+                onChanged: (value) => pesan = value,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 2, 88, 180),
+                  elevation: 1,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)
+                  )
+                ),
+                onPressed: submit, 
+                child: Text(
+                  'Kirim',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: Text(message),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              height: 40,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 33, 52, 72),
+                  elevation: 1,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)
+                  )
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                }, 
+                child: Text(
+                  'Kembali',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
